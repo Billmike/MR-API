1366eccc641cfc841ecaa1ba05dc38b6aec5046e
Ayelegun Kayode Michael <billmike1994@gmail.com>
2019-09-30 13:01:47 +0100

    Add enpoint for user signin

--- routes/users.js
+++ routes/users.js
@@ -4,5 +4,6 @@ const userController = require('../controllers/users.controllers');
 const user = Router();
 
 user.post('/signup', userController.createUser);
+user.post('/login', userController.loginUser);
 
 module.exports = user;
