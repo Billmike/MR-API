d773a138bad52788601692290c416a8deaae7969
Ayelegun Kayode Michael <billmike1994@gmail.com>
2019-10-01 08:37:08 +0100

    Complete skeleton for adding recipe

--- controllers/recipes.controllers.js
+++ controllers/recipes.controllers.js
@@ -16,7 +16,7 @@ const createRecipe = (request, response) => {
     user,
   } = request;
 
-  console.log('What user is', user);
+  const owner = user[0].user_id;
 
   const selectQuery = {
     text: 'SELECT * FROM recipes WHERE name = $1',
@@ -24,7 +24,7 @@ const createRecipe = (request, response) => {
   };
 
   const insertQuery = {
-    text: 'INSERT INTO recipes(recipe_id, name, description, category, cook_time, image_url, ingredients, directions, portion, owner) VALUES($1, $2, $3, $4, $5, $6, $7, $8, $9, $10)',
+    text: 'INSERT INTO recipes(recipe_id, name, description, category, cook_time, image_url, ingredients, directions, portion, owner) VALUES($1, $2, $3, $4, $5, $6, $7, $8, $9, $10) RETURNING *',
     values: [
       v4(),
       name,
@@ -35,8 +35,34 @@ const createRecipe = (request, response) => {
       ingredients,
       directions,
       portion,
+      owner,
     ],
   };
+
+  return db.query(selectQuery)
+    .then((existingRecipe) => {
+      if (existingRecipe.length > 0) {
+        return response.status(409).json({
+          success: false,
+          message: 'A recipe with this name has been created. We encourage our users to be unique in naming their recipes!',
+        });
+      }
+
+      return db.query(insertQuery)
+        .then((recipe) => {
+          response.status(201).json({
+            success: true,
+            message: 'Recipe created successfully',
+            recipe,
+          });
+        });
+    })
+    .catch(() => {
+      response.status(500).json({
+        success: false,
+        message: 'An error occurred while trying to complete your request. Please try again',
+      });
+    });
 };
 
 module.exports = {
--- routes/recipe.js
+++ routes/recipe.js
@@ -1,6 +1,9 @@
 const { Router } = require('express');
 const recipeController = require('../controllers/recipes.controllers');
+const verifySessionMiddleware = require('../middleware/loginRequired');
 
 const recipe = Router();
 
-recipe.post('/', recipeController.createRecipe);
+recipe.post('/', verifySessionMiddleware, recipeController.createRecipe);
+
+module.exports = recipe;
